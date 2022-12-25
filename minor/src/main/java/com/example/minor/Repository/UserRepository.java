18e2ab6d44a91b5b253a2cfc1261f25e293b7154
Sohil Khanduja <khandujasohil@gmail.com>
2022-12-25 18:08:35 +0530

    ak

--- minor/src/main/java/com/example/minor/Repository/UserRepository.java
+++ minor/src/main/java/com/example/minor/Repository/UserRepository.java
@@ -5,6 +5,4 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.example.minor.Model.User;
 public interface UserRepository extends MongoRepository<User, String> {
     Optional <User> findByEmail(String email);
-    Boolean existsByEmail(String email);
-     Boolean existsByName(String name);
 }
